30c047a355134983ef78335943c3767b2715440a
Adrian Lara <adrianblara@gmail.com>
2017-12-13 10:02:50 -0700

    Search test improvements: remove comments, fill in correct field on page

--- spec/features/user_can_search_for_closest_stations.rb
+++ spec/features/user_can_search_for_closest_stations.rb
@@ -3,21 +3,15 @@ require 'rails_helper'
 
 describe "When a user visits the root, fills in the search form with 80203, and clicks locate" do
   it "the user should see a list of the 10 closest stations (and their information) within 6 miles sorted by distance limited to Electric and Propane" do
-    # As a user
     visit '/'
-    # When I visit "/"
 
-    fill_in "form_name", with: "80203"
-    # And I fill in the search form with 80203
-
+    fill_in "q", with: "80203"
     click_on "Locate"
-    # And I click "Locate"
 
     expect(current_path).to eq(search_path)
-    # Then I should be on page "/search" with parameters visible in the url
 
     expect(page).to have_content("Stations (10)")
-    # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+
     expect(page).to have_content("UDR")
     expect(page).to have_content("800 Acoma St")
     expect(page).to have_content("Electric")
@@ -29,13 +23,11 @@ describe "When a user visits the root, fills in the search form with 80203, and 
     expect(page).to have_content("Electric")
     expect(page).to have_content("0.77306 miles")
     expect(page).to have_content("Employee use only")
-    # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
 
     expect(page).not_to have_content("Biodiesel (B20 and above)")
     expect(page).not_to have_content("Compressed Natural Gas")
     expect(page).not_to have_content("Ethanol (E85)")
     expect(page).not_to have_content("Hydrogen")
     expect(page).not_to have_content("Liquefied Natural Gas")
-    # And the stations should be limited to Electric and Propane
   end
 end
