0253c222b2c830ab17de7d89cc526b09e90e360c
Adrian Lara <adrianblara@gmail.com>
2017-12-13 09:52:52 -0700

    Test expectations more clearly defined

--- spec/features/user_can_search_for_closest_stations.rb
+++ spec/features/user_can_search_for_closest_stations.rb
@@ -16,9 +16,26 @@ describe "When a user visits the root, fills in the search form with 80203, and 
     expect(current_path).to eq(search_path)
     # Then I should be on page "/search" with parameters visible in the url
 
-    expect(page).to have_content()
+    expect(page).to have_content("Stations (10)")
     # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-    # And the stations should be limited to Electric and Propane
+    expect(page).to have_content("UDR")
+    expect(page).to have_content("800 Acoma St")
+    expect(page).to have_content("Electric")
+    expect(page).to have_content("0.31422 miles")
+    expect(page).to have_content("24 hours daily")
+
+    expect(page).to have_content("City of Denver - Wellington E Webb Municipal Building")
+    expect(page).to have_content("202 W Colfax Ave")
+    expect(page).to have_content("Electric")
+    expect(page).to have_content("0.77306 miles")
+    expect(page).to have_content("Employee use only")
     # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+
+    expect(page).not_to have_content("Biodiesel (B20 and above)")
+    expect(page).not_to have_content("Compressed Natural Gas")
+    expect(page).not_to have_content("Ethanol (E85)")
+    expect(page).not_to have_content("Hydrogen")
+    expect(page).not_to have_content("Liquefied Natural Gas")
+    # And the stations should be limited to Electric and Propane
   end
 end
