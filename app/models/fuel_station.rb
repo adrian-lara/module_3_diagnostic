4b75534304f6eb117a1d8cf5e0921498de9efac1
Adrian Lara <adrianblara@gmail.com>
2017-12-13 10:42:45 -0700

    fix bug where fuel station model was initializing by searching a hash for strings vs symbols

--- app/models/fuel_station.rb
+++ app/models/fuel_station.rb
@@ -3,11 +3,11 @@ class FuelStation
   attr_reader :name, :address, :fuel_type_code, :distance, :access_times
 
   def initialize(info)
-    @name = info["name"]
-    @address = info["address"]
-    @fuel_type_code = info["fuel_type_code"]
-    @distance = info["distance"]
-    @access_times = info["access_times"]
+    @name = info[:name]
+    @address = info[:address]
+    @fuel_type_code = info[:fuel_type_code]
+    @distance = info[:distance]
+    @access_times = info[:access_times]
   end
 
 end
