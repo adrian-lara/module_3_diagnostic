8fc17d946262b644976963f59d4dae249978bfea
Adrian Lara <adrianblara@gmail.com>
2017-12-13 10:59:33 -0700

    model test written for fuel station spec

---
+++ spec/models/fuel_station_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+describe FuelStation do
+  context "Instance Variables" do
+    context ".search()" do
+      it "returns an array of 10 Fuel Station objects" do
+        result = FuelStation.search("80203")
+
+        expect(result.count).to eq(10)
+        expect(result.first.name).to eq("UDR")
+        expect(result.last.name).to eq("City of Denver - Wellington E Webb Municipal Building")
+      end
+    end
+
+  end
+end
