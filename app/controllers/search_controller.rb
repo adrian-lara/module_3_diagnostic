9d8ef3598a3270b3f3fe58a531dd70c385abdd81
Adrian Lara <adrianblara@gmail.com>
2017-12-13 10:23:25 -0700

    Dream driving in search#index atm

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,19 @@
 class SearchController < ApplicationController
 
   def index
+    response = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?limit=10&&api_key=#{ENV['NREL_KEY']}&format=JSON&location=80203&fuel_type=LPG,ELEC&radius=6.0")
+    raw_fuel_stations = JSON.parse(response.body)["fuel_stations"]
+
+    @fuel_stations = raw_fuel_stations.map do |fuel_station_info|
+      binding.pry
+      FuelStation.create({
+        name: fuel_station_info["staton_name"],
+        address: fuel_station_info["street_address"],
+        fuel_type_code: fuel_station_info["fuel_type_code"],
+        distance: fuel_station_info["distance"], #TODO distance is relative to current query??? Will keep as is for now
+        address: fuel_station_info["access_days_time"]
+      })
+    end
 
   end
 
