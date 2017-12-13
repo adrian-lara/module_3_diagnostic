d45c63bf12c0f305f9d2c7e39da8b6aa9b83ac86
Adrian Lara <adrianblara@gmail.com>
2017-12-13 11:05:42 -0700

    add extra assertion in fuel station model test

--- spec/models/fuel_station_spec.rb
+++ spec/models/fuel_station_spec.rb
@@ -7,6 +7,7 @@ describe FuelStation do
         result = FuelStation.search("80203")
 
         expect(result.count).to eq(10)
+        expect(result.first).to be_a FuelStation
         expect(result.first.name).to eq("UDR")
         expect(result.last.name).to eq("City of Denver - Wellington E Webb Municipal Building")
       end
